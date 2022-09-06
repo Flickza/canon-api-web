2530d09d6c693d7cf601fd056f31b04dcff44c55
Flickza <Adelholmg@gmail.com>
2022-09-06 09:31:54 +0200

    ...

--- controllers/camera.js
+++ controllers/camera.js
@@ -3,7 +3,10 @@ import gphoto2 from 'gphoto2';
 export const capture = (req, res) => {
     try {
         var camera = req.app.get(camera);
-        console.log(camera);
+        // Take picture without downloading immediately
+        camera.takePicture({ download: false }, function (er, path) {
+            console.log(path);
+        });
     } catch (error) {
         res.status(404).json({ message: error.message });
     }
