043fb4a3062306d000110125f39617a67afd9d41
Flickza <Adelholmg@gmail.com>
2022-09-06 09:44:19 +0200

    ...

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.use(express.static("./" + 'src'));
 
 //Routes
 app.use('/', homeRoute);
-app.use('/camera', cameraRoute)
+app.use('/camera', camera, cameraRoute)
 
 app.listen(process.env.PORT, () => {
     console.log(`Server listening on port ${process.env.PORT}`)
--- controllers/camera.js
+++ controllers/camera.js
@@ -1,12 +1,11 @@
-import gphoto2 from 'gphoto2';
-
 export const capture = (req, res) => {
     try {
+        console.log(req);
         var camera = req.app.get(camera);
-        // Take picture without downloading immediately
-        camera.takePicture({ download: false }, function (er, path) {
-            console.log(path);
-        });
+        // // Take picture without downloading immediately
+        // camera.takePicture({ download: false }, function (er, path) {
+        //     console.log(path);
+        // });
     } catch (error) {
         res.status(404).json({ message: error.message });
     }
