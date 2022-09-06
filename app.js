62bea25df454e8142ed79aecf5c18075f4266908
Flickza <Adelholmg@gmail.com>
2022-09-06 08:35:22 +0200

    app

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 
 //routes
 import homeRoute from './routes/index.js';
+import cameraRoute from './routes/camera.js';
 
 //create express app
 const app = express();
@@ -21,8 +22,9 @@ app.use(express.urlencoded({ limit: '50mb', extended: true }));
 //set static folder location route
 app.use(express.static("./" + 'src'));
 
-//home routes
+//Routes
 app.use('/', homeRoute);
+app.use('/camera', cameraRoute)
 
 app.listen(process.env.PORT, () => {
     console.log(`Server listening on port ${process.env.PORT}`)
